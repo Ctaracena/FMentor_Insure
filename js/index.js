7f6bf9aa92802cf9bba1a9c1c5cc982e57ab0f8c
Carlos Taracena <charlietaracenadrums@gmail.com>
2020-10-10 18:05:47 -0500

    header terminado con menu mobile

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const menuBtn = document.querySelector('.menu')
+const mobileNav = document.querySelector('.mobile-nav')
+
+mobileNav.style.display = 'none'
+
+menuBtn.addEventListener('click', () => {
+  if (mobileNav.style.display === 'none') {
+    mobileNav.style.display = 'flex'
+    mobileNav.style.animationName = 'slide-in-right'
+    menuBtn.src = '../images/icon-close.svg'
+  } else {
+    mobileNav.style.display = 'none'
+    menuBtn.src = '../images/icon-hamburger.svg'
+  }
+})
